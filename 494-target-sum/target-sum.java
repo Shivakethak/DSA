70971b14f810e73b617b0415b4a93c01a2267167
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-03-20 11:36:42 +0530

    Time: 4 ms (86.13%) | Memory: 44.9 MB (49.36%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -8,18 +8,22 @@ class Solution {
         if(sum<target) return 0;
         if((sum-target)%2!=0) return 0;
         int newtarget = (sum-target)/2;
-        return findTargetSumWays(n-1,newtarget,nums);
+        int[][] dp = new int[n][newtarget+1];
+        for(int i =0;i<n;i++) Arrays.fill(dp[i],-1);
+        return findTargetSumWays(n-1,newtarget,nums,dp);
     }
-    public int findTargetSumWays(int i,int target,int[]nums){
+    public int findTargetSumWays(int i,int target,int[]nums,int[][] dp){
          if(i==0){
             if(target ==0 && nums[0]==0) return 2;
             else if(target==0 || nums[0]==target) return 1;
             else return 0;
          }
-         int nottake = findTargetSumWays(i-1,target,nums);
+         if( dp[i][target]!=-1) return  dp[i][target];
+         int nottake = findTargetSumWays(i-1,target,nums,dp);
          int take = 0;
-         if(nums[i]<=target) take = findTargetSumWays(i-1,target-nums[i],nums);
+         if(nums[i]<=target) take = findTargetSumWays(i-1,target-nums[i],nums,dp);
          int total = nottake + take ;
-         return total;
+         dp[i][target] = total;
+         return dp[i][target];
     }
 }
