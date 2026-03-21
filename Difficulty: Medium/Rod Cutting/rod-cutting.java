778ebae81d32c28ba76790fcb9c124480e34a67c
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-03-21 21:37:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.java
+++ Difficulty: Medium/Rod Cutting/rod-cutting.java
@@ -1,24 +1,24 @@
 class Solution {
     public int cutRod(int[] price) {
       int n = price.length;
-      int[] Price = new int[n+1];
-      int[][] dp = new int[n+1][n+1];
-      for(int i = 0;i<=n;i++) Arrays.fill(dp[i],-1);
-      for(int i =0;i<n;i++){
-          Price[i+1]=price[i];
-      }
-      return cutRod(n,n,Price,dp);
+    //   int[] Price = new int[n+1];
+      int[][] dp = new int[n][n+1];
+      for(int i = 0;i<n;i++) Arrays.fill(dp[i],-1);
+    //   for(int i =0;i<n;i++){
+    //       Price[i+1]=price[i];
+    //   }
+      return cutRod(n-1,n,price,dp);
         
     }
-    public int cutRod(int i,int rl,int[] Price,int[][] dp){
-        if(i==1){
-            if(rl>=1) return (rl/i)*Price[1];
+    public int cutRod(int i,int rl,int[] price,int[][] dp){
+        if(i==0){
+            if(rl>=1) return rl*price[0];
             else return 0;
         }
         if(dp[i][rl]!=-1) return dp[i][rl];
-        int nottake = cutRod(i-1,rl,Price,dp);
+        int nottake = cutRod(i-1,rl,price,dp);
         int take = Integer.MIN_VALUE;
-        if(i<=rl) take = Price[i]+cutRod(i,rl-i,Price,dp);
+        if((i+1)<=rl) take = price[i]+cutRod(i,rl-(i+1),price,dp);
         dp[i][rl] =Math.max(nottake,take);
         return dp[i][rl];
     }
