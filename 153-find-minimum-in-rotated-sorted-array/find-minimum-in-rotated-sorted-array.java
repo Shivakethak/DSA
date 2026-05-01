0506f2a625d91880848d2881645d218314ce227c
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-05-01 18:55:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (55.24%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,31 +1,45 @@
 class Solution {
     public int findMin(int[] nums) {
         int n = nums.length;
-        int k = n;
-        for(int i =0;i<n-1;i++){
-            if(nums[i+1]<nums[i]){
-               k = i+1;
-               break;
+        int low = 0;
+        int high = n-1;
+        int ans = Integer.MAX_VALUE;
+        while(low<=high){
+            int mid  = low+(high-low)/2;
+            if(nums[mid]<=nums[high]){
+                ans = Math.min(ans,nums[mid]);
+                high = mid-1;
+            }else{
+                ans = Math.min(ans,nums[low]);
+                low = mid+1;
             }
         }
-        reverse(0,k-1,nums);
-        reverse(k,n-1,nums);
-        return nums[n-1];
+        return ans;
+        // int k = n;
+        // for(int i =0;i<n-1;i++){
+        //     if(nums[i+1]<nums[i]){
+        //        k = i+1;
+        //        break;
+        //     }
+        // }
+        // reverse(0,k-1,nums);
+        // reverse(k,n-1,nums);
+        // return nums[n-1];
     }
-    public int[] reverse(int a,int b ,int[]nums){
-        int left= a;
-        int right = b;
-        while(left<right){
-        swap(left,right,nums);
-        left++;
-        right--;
-    }
-    return nums;
-    }
-    public int[] swap(int a,int b,int[] nums){
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-        return nums;
-    }
+    // public int[] reverse(int a,int b ,int[]nums){
+    //     int left= a;
+    //     int right = b;
+    //     while(left<right){
+    //     swap(left,right,nums);
+    //     left++;
+    //     right--;
+    // }
+    // return nums;
+    // }
+    // public int[] swap(int a,int b,int[] nums){
+    //     int temp = nums[a];
+    //     nums[a] = nums[b];
+    //     nums[b] = temp;
+    //     return nums;
+    // }
 }
