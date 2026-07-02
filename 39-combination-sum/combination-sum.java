19ad32185f9fb99458fafb8929a1600e7f3dbef1
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-07-02 23:44:38 +0530

    Time: 2 ms (96.11%) | Memory: 45.8 MB (33.47%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,32 +1,21 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(candidates, target, 0, new ArrayList<>(), result);
+        List<Integer> temp = new ArrayList<>();
+        combinationSum(0,target,candidates,temp,result);
         return result;
     }
-
-    private void backtrack(int[] candidates, int target, int start, 
-                           List<Integer> current, List<List<Integer>> result) {
-        
-        if (target == 0) {
-            result.add(new ArrayList<>(current));
-            return;
+    public void combinationSum(int i,int target,int[] candidates,List<Integer> temp,List<List<Integer>> res){
+        if(i==candidates.length) return;
+        if(target==0){
+         res.add(new ArrayList<>(temp));
+         return;
         }
-
-        if (target < 0) {
-            return;
+        if(candidates[i]<=target) {
+            temp.add(candidates[i]);
+            combinationSum(i,target-candidates[i],candidates,temp,res);
+            temp.remove(temp.size()-1);
         }
-
-        for (int i = start; i < candidates.length; i++) {
-            current.add(candidates[i]);
-
-            // i, not i+1 because same number can be reused
-            backtrack(candidates, target - candidates[i], i, current, result);
-
-            // backtrack
-            current.remove(current.size() - 1);
-        }
-    }
+        combinationSum(i+1,target,candidates,temp,res);
+}
 }
