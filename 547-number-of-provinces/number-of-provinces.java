2dac82ceed3f7811570b55b14582a3f229397127
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-07-13 17:44:36 +0530

    Time: 1 ms (87.67%) | Memory: 47.4 MB (31.54%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,30 +1,29 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        // ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int n = isConnected.length;
-        for(int i =0;i<n;i++) adj.add(new ArrayList<Integer>());
-        for(int i=0;i<n;i++){
-            for(int j =0;j<n;j++){
-                 if(i!=j && isConnected[i][j]==1) {
-                    adj.get(i).add(j);
-            }
-        }
-        }
+        // for(int i =0;i<n;i++) adj.add(new ArrayList<Integer>());
+        // for(int i=0;i<n;i++){
+        //     for(int j =0;j<n;j++){
+        //          if(i!=j && isConnected[i][j]==1) {
+        //             adj.get(i).add(j);
+        //     }
+        // }
         boolean[] vis = new boolean[n];
         int count = 0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
                 count+=1;
-                dfs(i,vis,adj);
+                dfs(i,isConnected,vis);
             }
         }
         return count;    
     }
-    private void dfs(int i,boolean[] vis,ArrayList<ArrayList<Integer>> adj){
+    private void dfs(int i,int[][] isConnected,boolean[] vis){
         vis[i] = true;
-        for(int n :adj.get(i)){
-            if(!vis[n]){
-                dfs(n,vis,adj);
+        for(int j = 0;j<isConnected.length;j++){
+            if(isConnected[i][j]==1 && !vis[j]){
+                dfs(j,isConnected,vis);
             }
         }
     }
