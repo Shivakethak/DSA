decbe41489a60de10f675dcee4df6eb1ba0e69dd
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-07-30 11:08:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Course Schedule I/course-schedule-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canFinish(int n, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
+        ArrayList<Integer> topo = new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        int[] indegree = new int[n];
+        for(int[] edge : prerequisites){
+            int u = edge[0];
+            int v = edge[1];
+            indegree[v]++;
+            adj.get(u).add(v);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++) if(indegree[i]==0) q.offer(i);
+        while(!q.isEmpty()){
+            int node = q.poll();
+            topo.add(node);
+            for(int neigh : adj.get(node)){
+                indegree[neigh]--;
+                if(indegree[neigh]==0) q.offer(neigh);
+            }
+        }
+        if(topo.size()==n) return true;
+        else return false;
+        
+    }
+}
