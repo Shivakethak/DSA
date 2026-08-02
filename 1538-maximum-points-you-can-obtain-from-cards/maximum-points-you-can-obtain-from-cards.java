6fc5bd3bf024b3e8eeb657dd53725c55d20ef6d9
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-08-02 14:17:02 +0530

    Time: 3 ms (32.68%) | Memory: 62 MB (17.23%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -1,19 +1,33 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int n = cardPoints.length;
-        int[] prefixsum = new int[n];
-        int[] backprefixsum = new int[n];
-        prefixsum[0] = cardPoints[0];
-        backprefixsum[n-1] = cardPoints[n-1];
-        for(int i=1;i<n;i++) prefixsum[i] = prefixsum[i-1]+cardPoints[i];
-        for(int i=n-2;i>=0;i--) backprefixsum[i] = backprefixsum[i+1]+cardPoints[i];
-        int max = Math.max(prefixsum[k-1],backprefixsum[n-k]);
-        int j = n-1;
-        for(int i =k-2;i>=0;i--){
-         int sum = prefixsum[i]+backprefixsum[j];
-         max = Math.max(max,sum);
-         j--;
-        }
-        return max;
+        // int[] prefixsum = new int[n];
+        // int[] backprefixsum = new int[n];
+        // prefixsum[0] = cardPoints[0];
+        // backprefixsum[n-1] = cardPoints[n-1];
+        // for(int i=1;i<n;i++) prefixsum[i] = prefixsum[i-1]+cardPoints[i];
+        // for(int i=n-2;i>=0;i--) backprefixsum[i] = backprefixsum[i+1]+cardPoints[i];
+        // int max = Math.max(prefixsum[k-1],backprefixsum[n-k]);
+        // int j = n-1;
+        // for(int i =k-2;i>=0;i--){
+        //  int sum = prefixsum[i]+backprefixsum[j];
+        //  max = Math.max(max,sum);
+        //  j--;
+        // }
+        // return max;
+    int leftsum = 0;
+    for (int i = 0; i < k; i++) {
+        leftsum += cardPoints[i];
     }
+    int max = leftsum;
+    int rightsum = 0;
+
+    for (int i = 0; i < k; i++) {
+        leftsum  -= cardPoints[k - 1 - i];
+        rightsum += cardPoints[n - 1 - i];
+        max = Math.max(max, leftsum + rightsum);
+    }
+
+    return max;
 }
+    }
