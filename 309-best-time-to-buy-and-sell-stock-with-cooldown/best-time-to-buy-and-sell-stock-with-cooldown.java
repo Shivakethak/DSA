90d4eccf4f1d99b659830ca40675e3e8b10e70f0
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-08-06 02:01:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (65.15%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,24 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n+1][3];
-        for(int i=0;i<=n;i++) Arrays.fill(dp[i],-1);
-        return maxProfit(0,1,prices,dp);
-    }
-    private int maxProfit(int i,int buy,int[] prices,int[][] dp){
-        if(i>=prices.length) return 0;
-        if(dp[i][buy]!=-1) return dp[i][buy];
-        int profit;
-        if(buy==1){
-            int bought = -prices[i]+ maxProfit(i+1,0,prices,dp);
-            int notbought = maxProfit(i+1,1,prices,dp);
-            profit = Math.max(bought,notbought);
-        }else{
-            int sold = prices[i]+maxProfit(i+2,1,prices,dp);
-            int notsold = maxProfit(i+1,0,prices,dp);
-            profit = Math.max(sold,notsold);
+        int[] after1 = new int[2];
+        int[] after2 = new int[2];
+        for(int i=0;i<2;i++) after2[i] = 0;
+        for(int i=0;i<2;i++) after1[i] = 0;
+        for(int i=n-1;i>=0;i--){
+            int[] curr = new int[2];
+            for(int j=0;j<=1;j++){
+                int profit;
+                if(j==1){
+                    int bought = -prices[i]+ after2[0];
+                    int notbought =after2[1];
+                    profit = Math.max(bought,notbought);
+                }else{
+                    int sold = prices[i]+after1[1];
+                    int notsold = after2[0];
+                    profit = Math.max(sold,notsold);
+                }
+                curr[j] = profit;
+            }
+            after1 = after2;
+            after2 = curr;
         }
-        dp[i][buy] = profit;
-        return dp[i][buy];
+        return after2[1];
     }
 }
