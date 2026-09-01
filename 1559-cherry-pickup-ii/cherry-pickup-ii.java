4687bb2c4cad4335394fb1a18d1b0a1304ecd6d7
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-09-02 01:14:50 +0530

    Time: 50 ms (5.29%) | Memory: 46.8 MB (22.13%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -2,14 +2,15 @@ class Solution {
     public int cherryPickup(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][][] dp = new int[m][n][n];
+        int[][] prev = new int[n][n];
         for(int i=0;i<n;i++){
             for(int j =0;j<n;j++){
-            if(i==j) dp[m-1][i][j] =  grid[m-1][i];
-            else dp[m-1][i][j] = grid[m-1][i]+grid[m-1][j];
+            if(i==j) prev[i][j] =  grid[m-1][i];
+            else prev[i][j] = grid[m-1][i]+grid[m-1][j];
         }
         }
         for(int i=m-2;i>=0;i--){
+            int[][] curr = new int[n][n];
             for(int j1= n-1;j1>=0;j1--){
                 for(int j2=n-1;j2>=0;j2--){
                     int max = Integer.MIN_VALUE;
@@ -17,37 +18,18 @@ class Solution {
                       for(int i1=0;i1<3;i1++){
                         for(int j3=0;j3<3;j3++){   
                             if(j1==j2) {
-                             if(j1+dj[i1]<n && j2+dj[j3]<n && j1+dj[i1]>=0 && j2+dj[j3]>=0)    max = Math.max(max,grid[i][j1]+dp[i+1][j1+dj[i1]][j2+dj[j3]]);
+                             if(j1+dj[i1]<n && j2+dj[j3]<n && j1+dj[i1]>=0 && j2+dj[j3]>=0)  max = Math.max(max,grid[i][j1]+prev[j1+dj[i1]][j2+dj[j3]]);
                             }
                             else  {
-                            if(j1+dj[i1]<n && j2+dj[j3]<n && j1+dj[i1]>=0 && j2+dj[j3]>=0) max = Math.max(max,grid[i][j1]+grid[i][j2]+dp[i+1][j1+dj[i1]][j2+dj[j3]]);
+                            if(j1+dj[i1]<n && j2+dj[j3]<n && j1+dj[i1]>=0 && j2+dj[j3]>=0) max = Math.max(max,grid[i][j1]+grid[i][j2]+prev[j1+dj[i1]][j2+dj[j3]]);
                             }
                         }
                       }
-                        dp[i][j1][j2] =  max;
+                        curr[j1][j2] =  max;
                     }  
                 }
+                prev = curr;
             }
-        return dp[0][0][n-1];
+        return prev[0][n-1];
     }
-    // public int cherryPickup(int i,int j1,int j2,int[][] grid,int[][][] dp){
-    //     int m = grid.length;
-    //     int n = grid[0].length;
-    //     if(j1<0||j1>=n||j2<0||j2>=n) return (int)-1e8;
-    //     if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-    //     if(i==m-1){
-    //         if(j1==j2) return grid[i][j1];
-    //         else return grid[i][j1]+grid[i][j2];
-    //     }
-    //     int max = Integer.MIN_VALUE;
-    //     int[] dj = {-1,0,1};
-    //     for(int i1=0;i1<3;i1++){
-    //         for(int j3=0;j3<3;j3++){
-    //         if(j1==j2) max = Math.max(max,grid[i][j1]+cherryPickup(i+1,j1+dj[i1],j2+dj[j3],grid,dp));
-    //         else  max = Math.max(max,grid[i][j1]+grid[i][j2]+cherryPickup(i+1,j1+dj[i1],j2+dj[j3],grid,dp));
-    //     }
-    //     }
-    //     dp[i][j1][j2] =  max;
-    //     return dp[i][j1][j2];
-    // }
 }
