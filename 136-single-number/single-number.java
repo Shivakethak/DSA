5142761ee629ac493f466703aafe6edf96396e68
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-09-10 15:33:57 +0530

    Time: 7 ms (27.55%) | Memory: 47.1 MB (27.96%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,14 +1,35 @@
-class Solution {
+// class Solution {
+//     public int singleNumber(int[] nums) {
+        // Map<Integer,Integer> map = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+        // for(int key :map.keySet()){
+        //     if(map.get(key)<2){
+        //        return key;
+        //     }
+        // }
+        // return -1;
+    class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        for(int key :map.keySet()){
-            if(map.get(key)<2){
-               return key;
+
+        int ans = 0;
+
+        for (int bitIndex = 0; bitIndex < 32; bitIndex++) {
+
+            int cnt = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                if ((nums[i] & (1 << bitIndex)) != 0) {
+                    cnt++;
+                }
+            }
+
+            if (cnt % 2 != 0) {
+                ans = ans | (1 << bitIndex);
             }
         }
-        return -1;
+
+        return ans;
     }
 }
