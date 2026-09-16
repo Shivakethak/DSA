6d2b5f0a812ec24533a0d8d82bd41f3e57ed03f0
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-09-16 19:35:25 +0530

    Time: 44 ms (97.29%) | Memory: 115.8 MB (56.95%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -3,16 +3,13 @@ class Solution {
         Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
         int n = intervals.length;
         int count = 0;
-        int i = 1;
         int prevend = intervals[0][1];
-        while(i<n){
-        if(prevend>intervals[i][0]){
-        count++;
-        i++;
-        continue;
-        } 
-        prevend = intervals[i][1];
-        i++;
+        for(int i = 1;i<n;i++){
+           if(prevend>intervals[i][0]){
+           count++;
+           }else{
+           prevend = intervals[i][1];
+           }
         }
         return count;
     }
