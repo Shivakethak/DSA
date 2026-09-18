d135c06eff35f3c1e541815124d4eb9116cf6b7d
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-09-18 19:09:37 +0530

    Time: 2 ms (25.22%) | Memory: 47.2 MB (37.43%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,22 +1,40 @@
+class Pair{
+    int start;
+    int end ;
+    Pair(int start,int end){
+        this.start = start;
+        this.end = end;
+    }
+}
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
-        List<int[]> result = new ArrayList<>();
-        int  i =0;
-        while(i< n && newInterval[0] > intervals[i][1]){
-            result.add(intervals[i]);
-            i+=1;
+        ArrayList<Pair> res  = new ArrayList<>();
+        int i =0;
+        // no overlap
+        while(i<n && intervals[i][1] < newInterval[0]){
+        res.add(new Pair(intervals[i][0],intervals[i][1]));
+        i++;
         }
-        while(i<n && newInterval[1] >= intervals[i][0]){
-           newInterval[0] = Math.min(newInterval[0],intervals[i][0]);
-           newInterval[1]= Math.max(newInterval[1],intervals[i][1]);
-           i+=1;
+        // merging overlaping intervals
+        while(i<n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(newInterval[0],intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1],intervals[i][1]);
+            i++;
         }
-        result.add(newInterval);
+        // add the merged result to the ans
+        res.add(new Pair(newInterval[0],newInterval[1]));
         while(i<n){
-              result.add(intervals[i]);
-              i++;
+            res.add(new Pair(intervals[i][0],intervals[i][1]));
+            i++;
         }
-        return result.toArray(new int[result.size()][2]);
-    }
+        int N = res.size();
+        int[][] result = new int[N][2];
+        for(int k = 0;k<N;k++){
+            result[k][0] = res.get(k).start;
+            result[k][1] = res.get(k).end;
+        }
+        return result;
+
+                }
 }
