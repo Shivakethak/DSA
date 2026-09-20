8e1780897933cab0a3f7962b799f4a3f2727b9a9
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-09-20 14:34:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (82.70%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -3,35 +3,33 @@ class Solution {
         int n = s.length();
         Stack<Integer> S = new Stack<>();
         Stack<Integer> S1 = new Stack<>();
-        for(int i=0 ; i<n ; i++){
-            if(s.charAt(i)=='('){
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(') {
                 S.push(i);
-            }else if (s.charAt(i)==')'){
-                if(S.size()==0 && S1.size() == 0){
+            } else if (s.charAt(i) == ')') {
+                if (S.size() == 0 && S1.size() == 0) {
                     return false;
                 }
-                if(S.size() ==0 && S1.size()!=0){
-                        S1.pop();
-                        continue;
-                    }
-                if(S.size()!=0){
+                if (S.size() == 0 && S1.size() != 0) {
+                    S1.pop();
+                    continue;
+                }
+                if (S.size() != 0) {
                     S.pop();
                 }
-            }else{
+            } else {
                 S1.push(i);
             }
         }
-        if(S.size()==0) return true;
-        while(!S.isEmpty() && !S1.isEmpty()){
-          int index  = S.peek();
-          int index1 = S1.peek();
-          if(index1<index) return false;
-          else{ 
-            S.pop();
-            S1.pop();
-          }
+        if (S.size() == 0)
+            return true;
+        while (!S.isEmpty() && !S1.isEmpty()) {
+            int index = S.pop();
+            int index1 = S1.pop();
+            if (index1 < index){
+                return false;
+            }
         }
-        if(S.size()==0) return true;
-    return false;
-}
+        return S.isEmpty();
+    }
 }
