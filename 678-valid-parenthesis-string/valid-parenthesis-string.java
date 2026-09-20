2ad05f963c6758c2d88e159773bd4562cab80460
Shivakethak <bl.en.u4aid23014@bl.students.amrita.edu>
2026-09-20 16:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (67.81%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,32 +1,50 @@
 class Solution {
     public boolean checkValidString(String s) {
         int n = s.length();
-        Stack<Integer> S = new Stack<>();
-        Stack<Integer> S1 = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == '(') {
-                S.push(i);
-            } else if (s.charAt(i) == ')') {
-                if (!S.isEmpty()) {
-                     S.pop();
-                }
-                else if (S1.isEmpty()) {
-                    return false;
-                }
-                else{
-                    S1.pop();
-                }
-            }  else {
-                S1.push(i);
-            }
-        }
-        while (!S.isEmpty() && !S1.isEmpty()) {
-            int index = S.pop();
-            int index1 = S1.pop();
-            if (index1 < index){
-                return false;
-            }
-        }
-        return S.isEmpty();
+    //     Stack<Integer> S = new Stack<>();
+    //     Stack<Integer> S1 = new Stack<>();
+    //     for (int i = 0; i < n; i++) {
+    //         if (s.charAt(i) == '(') {
+    //             S.push(i);
+    //         } else if (s.charAt(i) == ')') {
+    //             if (!S.isEmpty()) {
+    //                  S.pop();
+    //             }
+    //             else if (S1.isEmpty()) {
+    //                 return false;
+    //             }
+    //             else{
+    //                 S1.pop();
+    //             }
+    //         }  else {
+    //             S1.push(i);
+    //         }
+    //     }
+    //     while (!S.isEmpty() && !S1.isEmpty()) {
+    //         int index = S.pop();
+    //         int index1 = S1.pop();
+    //         if (index1 < index){
+    //             return false;
+    //         }
+    //     }
+    //     return S.isEmpty();
+    // }
+    int leftcount = 0;
+    int rightcount = 0;
+    for(int i = 0; i<n; i++){
+        if((s.charAt(i)=='(')  || (s.charAt(i)=='*')) leftcount++;
+        else {
+            leftcount--;
+            if(leftcount<0) return false;
     }
+    }
+    for(int i = n-1; i>=0 ;i--){
+        if((s.charAt(i)==')') || (s.charAt(i)=='*')) rightcount++;
+        else{
+             rightcount--;
+             if(rightcount<0) return false;
+    }
+    }
+    return (leftcount >=0 && rightcount >=0);
+}
 }
